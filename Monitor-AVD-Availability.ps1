296daf37e92347fa366e3274492201992d81f3d6
cshayter <51821996+duipo@users.noreply.github.com>
2022-02-11 17:54:56 +0000

    Update Monitor-AVD-Availability.ps1

--- Monitor-AVD-Availability.ps1
+++ Monitor-AVD-Availability.ps1
@@ -1,11 +1,11 @@
 ﻿######### Secrets/Variables #########
-$ApplicationId = '2f73e4cd-e344-40a8-8e4f-c5b6f840aa69'
-$ApplicationSecret = 'PMEvk4sDfhj7NzAniViISWnM6RzT+b05GcSBeyTWBuE=' | ConvertTo-SecureString -Force -AsPlainText
-$TenantID = '68c4d87a-ce14-435f-add3-e9a6bc3b00e5'
-$RefreshToken = '0.ATsAetjEaBTOX0Ot0-mmvDsA5c3kcy9E46hAjk_FtvhAqmk7ACo.AgABAAAAAAD--DLA3VO7QrddgJg7WevrAgDs_wQA9P_dhokLNI9ZstfRC45kLSkFbCfMg3Z68L3tH31-UPVGcqvXJ1sYB2G2DOGgK96MonEmzjle5IRKcQGpXEF7iVxyzdOrmnZDkRwvgv4LDqWjcnPeLqOtJRlMSIxErRWXd-HqspWmm_SsRag6fFKsC_JOk1O45F7u6bFvTxD-8lzEnS2wu7cKGRZWDQOEFvxvpuY4eAH7VdMuhkEmkkH2xS14GsUgbQnoqojJ1ttHhs15d_QzfHIYVBSlEsvQNoYVlPXzLdjhHOCwUcY-WwN8q_FYrz6F1QjW8OXkA3Xr80iH4S5OIMYPQskvJZBZy-DggIuSXJjYCV0_IjZ_jcqvd2xundiA_fwk9awTCEMqqSqRc1u0beX8-qCyOf2mMnT8MNv18TClDVB2EqaL-FtOmXbzOVO88x5Ky-RoihI09Gjbs0_jdj9_7u8QFD0TkDSN-81CEE1mn6cIYk1auxE1e-GIqpIS94m-PSOrmlbRXZKsh1ETGkVULmrvW7CPuHktvBeRe4MR5Sbm5HYRbE6XZGgOe6swzL5j2TrjwEATzNEgJGILBpwvbn-esidi91VVB3a6On78O_AcwSVLzKYIWIWO9NNMlYjaygbJQDsaQWKGdH5pOFrVswnWEp9xAGoz-DRZGhnT4FUbj7TLqEVFr_Au9rwHSfjjEnnh0yq2DRLWPn2-28EG76rkChq0VMwzlOTwB_sud4CwnUD73XQaiQCyLTy1k-AV2qpFK89wnoXTyBP59wiqoVVXG-FDfsqdNONJ-tbr0Ux0c19dBd5tE3PU1M5gmDaQSsbyYU7_Bux5forquIWWMCqEXKmFnB7otCmrZDAfE1cTef6Q9p83Jqy2fuT8HBpflS7CwvPWXh2aLObvWVlMnu_vo9SgI_KsXUaE9SIpvwo9qN3xkkE0B9pJEklzVp-DJi43Q0qP3u5x8kesP_oG0ua-eTpUdfL9m6gMS5GI2-mjVT1BvZwfWZlfOvpt3NG446uOjdN9b7Kuafw8kvU9y-Z4jfkhmmYOD4xOdye_pUshzUBn2DRC-oeubfoqNLsH00soeCmbRzdQ6-cQnfTzWyxnkIGh34cnI1x8XgafeSqTLXIyFkuF7wt7amCPg0hhhDrMBJh8de6TMj7lqyU7YcVIA2oY9eW5TMxVa4yZ8LP7-Dk'
-$UPN = "matrix.admin@kingswood-group.com"
-$MinimumFreeGB = '50'
-$SubscriptionID = '05bf03b1-bbfe-481c-872d-6537536c80a1'
+$ApplicationId = 
+$ApplicationSecret = | ConvertTo-SecureString -Force -AsPlainText
+$TenantID =
+$RefreshToken = 
+$UPN
+$MinimumFreeGB =
+$SubscriptionID =
 ######### Secrets/Variables #########
 If (Get-Module -ListAvailable -Name Az.DesktopVirtualization) { 
     Import-module Az.DesktopVirtualization
